13ec149fe9606be8635eb00875fd312bb97dbe86
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-25 23:27:45 +0300

    defined constructor and methods for Order type

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+/* ==================== BUSINESS LOGIC ============================= */
 
+/* ---------- Order ------------- */
+
+// Order Constucter
+function Order() {
+    this.orderDate = new Date();
+    this.orderDetails = [];
+    this.delivery = {
+        deliveryContact: "",
+        deliveryAdress: "",
+        deliveryCost: 0,
+    };
+}
+
+/* ---------- Order Methods ------------- */
+
+// orderDetails cost summary
+Order.prototype.orderDetailsCost = function () {};
+
+// Order grand total
+Order.prototype.grandTotal = function () {};
