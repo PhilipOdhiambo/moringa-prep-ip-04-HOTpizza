97523c6fe17795f3d296d2dbc26bcf080d1ad697
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-26 00:10:16 +0300

    defined Pizza Constructor and it's cost method

--- js/script.js
+++ js/script.js
@@ -20,3 +20,36 @@ Order.prototype.orderDetailsCost = function () {};
 
 // Order grand total
 Order.prototype.grandTotal = function () {};
+
+/* ---------- orderDetail (PIZZA) ------------- */
+
+// Pizza Constructor
+function Pizza(size, crust, toppings, quantity) {
+    this.size = size;
+    this.crust = {
+        crustName: crust.name,
+        crustCost: crust.name,
+    };
+    this.toppings = toppings;
+    this.quantity = quantity;
+}
+
+// Pizza methods
+
+Pizza.prototype.cost = function () {
+    var costFactor;
+    if (this.size === "small") {
+        costFactor = 1;
+    } else if (this.size === "medium") {
+        costFactor = 1.5;
+    } else {
+        costFactor = 1;
+    }
+
+    var crustCost = this.crust.crustCost * costFactor * this.quantity;
+    var toppingsCost =
+        this.toppings.reduce((sum, topping) => sum + topping.cost) *
+        costFactor *
+        this.quantity;
+    return crustCost + toppingsCost;
+};
